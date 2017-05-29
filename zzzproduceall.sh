b278e386a9fa5a27e2e24636e92fddf46de06203
megaNour <alkotobnour@gmail.com>
2017-05-29 21:01:54 +0200

    menage

--- zzzproduceall.sh
+++
@@ -1,9 +0,0 @@
-find . -name "*.kra" -printf "%f\n" | 
-while read fileName; 
-do	base="$(cut -d "." -f1 <<< "$fileName")"
-	unzip "$base.kra" mergedimage.png;
-	mv mergedimage.png "$base.png"
-	convert "$base.png" -resize 990x1400  "$base.jpg"
-	mv "$base.png" ../png/
-	mv "$base.jpg" ../jpg/
-done
